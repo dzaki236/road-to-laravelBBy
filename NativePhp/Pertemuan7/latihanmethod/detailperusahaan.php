42d400a264897a257d701a1deb4dd43974d50784
Dzaki Ahnaf Zulfikar <dzakiahnafz@gmail.com>
2021-02-13 10:33:32 +0700

    added some stuff of little

--- NativePhp/Pertemuan7/latihanmethod/detailperusahaan.php
+++ NativePhp/Pertemuan7/latihanmethod/detailperusahaan.php
@@ -1,6 +1,51 @@
 <?php 
 
-echo $_GET["perusahaan"];
-echo $_GET["butuh"];
-echo $_GET["syarat"];
 ?>
+<!doctype html>
+<html lang="en">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <!-- Bootstrap CSS -->
+    
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-BmbxuPwQa2lc/FVzBcNJ7UAyJxM6wuqIj61tLrc4wSX0szH/Ev+nYRRuWlolflfl" crossorigin="anonymous">
+<style>
+        .img-thumbnail {
+            max-height: 20vh;
+            object-fit: contain;
+            border: none;
+        }
+
+        @import url("https://cdn.jsdelivr.net/npm/bootstrap-icons@1.3.0/font/bootstrap-icons.css");
+    </style>
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.3.0/font/bootstrap-icons.css">
+    <title>Hello, world!</title>
+  </head>
+  <body>
+  <div class="row container p-5">
+  <img src="<?= $_GET["profile"]?>" class="img-thumbnail" alt="..." >
+  <h1 class="fs-1"><?= $_GET["perusahaan"];?></h1>
+ 
+  <p> <i class="bi bi-clock-history"></i> lama kerja : <?= $_GET["lamanya"]?></p>
+  <p>Deskripsi perusahaan : </p>
+  <p class="text-secondary fw-lighter"><?= $_GET["desk"];?></p>
+  <p>Syarat : </p>
+  <ul class="fw-lighter">
+  <?= $_GET["syarat"]?>
+  </ul>
+  <a href="latihan-method.php" class="btn btn-outline-warning">Kirim lamaran mu</a>
+  </div>
+    <!-- Optional JavaScript; choose one of the two! -->
+
+    <!-- Option 1: Bootstrap Bundle with Popper -->
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta2/dist/js/bootstrap.bundle.min.js" integrity="sha384-b5kHyXgcpbZJO/tY9Ul7kGkf1S0CWuKcCD38l8YkeH8z8QjE0GmW1gYU5S9FOnJ0" crossorigin="anonymous"></script>
+
+    <!-- Option 2: Separate Popper and Bootstrap JS -->
+    <!--
+    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.6.0/dist/umd/popper.min.js" integrity="sha384-KsvD1yqQ1/1+IA7gi3P0tyJcT3vR+NdBTt13hSJ2lnve8agRGXTTyNaBYmCR/Nwi" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta2/dist/js/bootstrap.min.js" integrity="sha384-nsg8ua9HAw1y0W1btsyWgBklPnCUAFLuTMS2G72MMONqmOymq585AcH49TLBQObG" crossorigin="anonymous"></script>
+    -->
+  </body>
+</html>
--- NativePhp/Pertemuan7/latihanmethod/latihan-method.php
+++ NativePhp/Pertemuan7/latihanmethod/latihan-method.php
@@ -1,12 +1,11 @@
 <?php 
 $perusahaan = [
-    "pt.1" => ["Nama-pt"=>"Pt.test","dibutuhkan"=>"front end dev","lamawaktu"=>"6 bulan","gaji"=>"5juta","link"=>"Perusahaan-test3.com","pp"=>"https://ecs7.tokopedia.net/img/cache/700/product-1/2019/1/5/42164186/42164186_abd49369-460e-4163-b355-96cf75803f21_1000_1000.png","deskripsi"=>" Lorem ipsum dolor, sit amet consectetur adipisicing elit. Totam, molestiae. Laudantium vero aut veniam voluptatem! Voluptatum assumenda molestiae quo, obcaecati rem quisquam impedit quia, officiis, perferendis facere accusantium vitae pariatur!","syarat" => "memiliki kualifikasi tinggi"],
+    "pt.1" => ["Nama-pt"=>"Pt.test","dibutuhkan"=>"front end dev","lamawaktu"=>"6 bulan","gaji"=>"5juta","link"=>"Perusahaan-test3.com","pp"=>"https://ecs7.tokopedia.net/img/cache/700/product-1/2019/1/5/42164186/42164186_abd49369-460e-4163-b355-96cf75803f21_1000_1000.png","deskripsi"=>" Lorem ipsum dolor, sit amet consectetur adipisicing elit. Totam, molestiae. Laudantium vero aut veniam voluptatem! Voluptatum assumenda molestiae quo, obcaecati rem quisquam impedit quia, officiis, perferendis facere accusantium vitae pariatur!","syarat" => "<li>syarat 1</li><li>syarat 2</li><li>syarat 3</li>"],
 
-    "pt.2" => ["Nama-pt"=>"Pt.test2","dibutuhkan"=>"back end end dev","lamawaktu"=>"8 bulan","gaji"=>"10juta","link"=>"Perusahaan-test2.com","pp"=>"https://cdn.idntimes.com/content-images/post/20160710/p3-a2d6cf867ed1b50085aceb629fa38b1d.jpg","deskripsi"=>" Lorem ipsum dolor, sit amet consectetur adipisicing elit. Totam, molestiae. Laudantium vero aut veniam voluptatem! Voluptatum assumenda molestiae quo, obcaecati rem quisquam impedit quia, officiis, perferendis facere accusantium vitae pariatur!"],
+    "pt.2" => ["Nama-pt"=>"Pt.test2","dibutuhkan"=>"back end end dev","lamawaktu"=>"8 bulan","gaji"=>"10juta","link"=>"Perusahaan-test2.com","pp"=>"https://cdn.idntimes.com/content-images/post/20160710/p3-a2d6cf867ed1b50085aceb629fa38b1d.jpg","deskripsi"=>" Lorem ipsum dolor, sit amet consectetur adipisicing elit. Totam, molestiae. Laudantium vero aut veniam voluptatem! Voluptatum assumenda molestiae quo, obcaecati rem quisquam impedit quia, officiis, perferendis facere accusantium vitae pariatur!","syarat" => "<li>syarat 1</li><li>syarat 2</li><li>syarat 3</li>"],
 
-    "pt.3"=>["Nama-pt"=>"Pt.test3","dibutuhkan"=>"fullstack dev","lamawaktu"=>"12 bulan","gaji"=>"10juta","link"=>"Perusahaan-test1.com","pp"=>"https://s.kaskus.id/images/fjb/2015/04/16/jasa_pembuatan_desain_logo_perusahaan_murah_tidak_murahan_1157447_1429123045.JPG","deskripsi"=>" Lorem ipsum dolor, sit amet consectetur adipisicing elit. Totam, molestiae. Laudantium vero aut veniam voluptatem! Voluptatum assumenda molestiae quo, obcaecati rem quisquam impedit quia, officiis, perferendis facere accusantium vitae pariatur!"]
+    "pt.3"=>["Nama-pt"=>"Pt.test3","dibutuhkan"=>"fullstack dev","lamawaktu"=>"12 bulan","gaji"=>"10juta","link"=>"Perusahaan-test1.com","pp"=>"https://s.kaskus.id/images/fjb/2015/04/16/jasa_pembuatan_desain_logo_perusahaan_murah_tidak_murahan_1157447_1429123045.JPG","deskripsi"=>" Lorem ipsum dolor, sit amet consectetur adipisicing elit. Totam, molestiae. Laudantium vero aut veniam voluptatem! Voluptatum assumenda molestiae quo, obcaecati rem quisquam impedit quia, officiis, perferendis facere accusantium vitae pariatur!","syarat" => "<li>syarat 1</li><li>syarat 2</li><li>syarat 3</li>"]
 ];
-
 ?>
 <!doctype html>
 <html lang="en">
@@ -50,7 +49,7 @@ $perusahaan = [
                 <p style="font-size:14px;" class="fw-light text-secondary"><i class="bi bi-info h5"></i>
                     <?= " <b> Profile perusahaan </b>: <br>".$lamaran["deskripsi"]; ?></p>
             </div>
-            <a href = "./detailperusahaan.php?perusahaan=<?= $lamaran["Nama-pt"]?>&butuh=<?= $lamaran["dibutuhkan"]?>&syarat=<?= $lamaran["syarat"]?>" class="btn btn-link row mt-4">Lihat detail>></a>
+            <a href = "./detailperusahaan.php?perusahaan=<?= $lamaran["Nama-pt"]?>&butuh=<?= $lamaran["dibutuhkan"]?>&desk=<?= $lamaran["deskripsi"]?>&syarat=<?= $lamaran["syarat"]?>&lamanya=<?= $lamaran["lamawaktu"]?>&profile=<?= $lamaran["pp"]?>" class="btn btn-link row mt-4">Lihat detail>></a>
         </div>
     </div>
     <?php endforeach;?>
